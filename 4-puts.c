b1c9a28ef665ae21cce346a07ac611d082c33642
VALENTINE CHELUCHI AGBA <krixxvalle@gmail.com>
2023-02-16 18:02:51 -0800

    4puts

---
+++ 4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main : A program that stars tge process, its also a function
+ * return: 0, a program that ends the process
+ **/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
