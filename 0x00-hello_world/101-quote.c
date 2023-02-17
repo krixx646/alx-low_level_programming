ab437fa0e0304006697cf472f670aff01fc68c43
VALENTINE CHELUCHI AGBA <krixxvalle@gmail.com>
2023-02-17 08:36:36 -0800

    adv_task

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main -A A c program that prints a line to the standard error
- * Return 1 (Success)
+ * main - A program that prints a line using printf function
+ * Return: 0 (Success)
  */
+
 int main(void)
 {
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
