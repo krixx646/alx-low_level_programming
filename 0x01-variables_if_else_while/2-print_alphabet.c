71f806fddd2826f85a3c334d292cfd68b60c642b
VALENTINE CHELUCHI AGBA <krixxvalle@gmail.com>
2023-02-17 15:51:19 -0800

    alpha

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ *main-program entry point.
+ *Return:0 [successfully Executed]
+ **/
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
