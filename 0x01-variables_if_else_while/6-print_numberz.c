75fe1fa2d89b5ea7e99a88fd75f1ea9b2995cf79
VALENTINE CHELUCHI AGBA <krixxvalle@gmail.com>
2023-02-17 16:48:45 -0800

    C Variables

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main-program entry point.
+ *Return:0 Success code
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main-program entry point.
+ * Return: 0 Success code end
+ */
+int main(void)
+{
+	int i;
+	char hexvalues[] = "0123456789abcdef";
+
+	for (i = 0; i < 16; i++)
+	{
+		putchar(hexvalues[i]);
+	}
+	putchar('\n');
+	return (0);
+}
