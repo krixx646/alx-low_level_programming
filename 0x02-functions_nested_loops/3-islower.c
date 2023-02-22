d01427b5dbe57e975f21a3ccea9ebbc16bc155c4
Valentine Cheluchi <krixxvalle@gmail.com>
2023-02-22 01:12:53 +0000

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+include "main.h"
+/**
+ * _islower-check if character is lowercase.
+ * @c:character checker || pointer
+ * Return:0- true 1 false.
+ */
+int
+_islower(int c)
+{
+	if (j >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
