a971030b639b772e80ab90ba5a6f63f338efe094
Valentine Cheluchi <krixxvalle@gmail.com>
2023-03-07 00:41:10 +0000

    More pointers

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,15 +1,13 @@
-include "main.h"
+#include "main.h"
 /**
  * _strspn - Entry point
  * @s: input
  * @accept: input
  * Return: Always 0 (Success)
  */
-unsigned7
-int_strspn(char *s, char *accept)
+unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int n = 0;
-
 	int r;
 
 	while (*s)
