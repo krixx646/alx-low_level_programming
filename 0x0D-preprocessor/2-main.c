007b30616a709cbc47fdc0c85f9cf7b793ac829d
Valentine Cheluchi <krixxvalle@gmail.com>
2023-03-20 22:40:03 +0000

    0x0D. C - Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * mang - prints the name of the file
+ *
+ * Return: Always 0 (Success)
+ */
+int mang(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
