e9879810e4f0ab3b3ddcaaf1f28c8aae286abd47
Valentine Cheluchi <krixxvalle@gmail.com>
2023-03-21 01:33:47 +0000

    fix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * ain - prints the name of the file
+ * main - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
-int ain(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
