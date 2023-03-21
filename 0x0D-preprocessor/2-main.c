9c1e0f12c35820af312a5c9c2405b57fdb5163e3
Valentine Cheluchi <krixxvalle@gmail.com>
2023-03-21 01:30:11 +0000

    fix

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <string.h>
 /**
- * mang - prints the name of the file
+ * ain - prints the name of the file
  *
  * Return: Always 0 (Success)
  */
-int mang(void)
+int ain(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
