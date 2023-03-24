96cbfcd28b60b4aeb68f443690ee836eeb1e4a41
Valentine Cheluchi <krixxvalle@gmail.com>
2023-03-24 00:51:59 +0000

    Variadic Functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,13 +11,21 @@
  */
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list ap;
+	va_list args;
+	unsigned int i;
+	int sum = 0;
 
-	unsigned int i, sum = 0;
+	if (n == 0)
+		return (0);
 
-	va_start(ap, n);
+	va_start(args, n);
+
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
-	va_end(ap);
+	{
+		sum += va_arg(args, int);
+	}
+
+	va_end(args);
+
 	return (sum);
 }
