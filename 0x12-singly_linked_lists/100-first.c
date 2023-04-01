0433134c97cc8b65885deca84db33383b8ed0575
krixx Valentine <80608660+krixx646@users.noreply.github.com>
2023-04-01 16:50:57 +0100

    Hot fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * bmain - function executed before main
- * Return: no return.
+ * bmain - function 
+ * Return: (void) no return.
  */
 
 void __attribute__ ((constructor)) bmain()
