caf47a88177ded70d80600767ebcb1aa60ef58db
Valentine Cheluchi Agba <krixxvalle@gmail.com>
2023-04-06 05:01:11 +0000

    fix

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -5,36 +5,32 @@
  *
  * Return: The size of the list that was free'd
  */
-
 size_t free_listint_safe(listint_t **h)
 {
-    size_t len = 0;
-    int difference;
-    listint_t *temp_ptr;
+	size_t lake = 0;
+	int difference;
+	listint_t *temp;
 
-    if (!h || !*h)
-        return (0);
-
-    while (*h)
-    {
-        difference = *h - (*h)->next;
-        if (difference > 0)
-        {
-            temp_ptr = (*h)->next;
-            free(*h);
-            *h = temp_ptr;
-            len++;
-        }
-        else
-        {
-            free(*h);
-            *h = NULL;
-            len++;
-            break;
-        }
-    }
-
-    *h = NULL;
-    return (len);
+	if (!h || !*h)
+		return (0);
+	while (*h)
+	{
+		difference = *h - (*h)->next;
+		if (difference > 0)
+		{
+			temp = (*h)->next;
+			free(*h);
+			*h = temp;
+			lake++;
+		}
+		else
+		{
+			free(*h);
+			*h = NULL;
+			lake++;
+			break;
+		}
+	}
+	*h = NULL;
+	return (lake);
 }
-
